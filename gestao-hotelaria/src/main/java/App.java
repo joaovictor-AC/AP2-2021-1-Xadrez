3f724c20634bfb67a8003dc35750f97add2545e2
Vicente José de Araújo Neto <88285241+Sothenheim@users.noreply.github.com>
2021-10-04 16:12:41 -0300

    Add files via upload

---
+++ gestao-hotelaria/src/main/java/App.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vicen
+ */
+public class App {
+    public static void main(String[]args){
+        ArrayList<String> usuario=new ArrayList<String>();
+        ArrayList<String> senha=new ArrayList<String>();
+        Scanner in=new Scanner(System.in);
+        int x=0;
+        x = in.nextInt();
+        System.out.println(x);
+        
+    }
+    
+}
