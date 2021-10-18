985702186571e7af0ae274edc3f4807dd79628e7
João Victor <jvaraujocosta13@gmail.com>
2021-10-18 13:04:09 -0300

    Implementando calendário no sistema

--- Gestão Hotel/src/Reserva.java
+++ Gestão Hotel/src/Reserva.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,13 +16,12 @@ public class Reserva {
 
         while (quarto_encontrado) {
 
-            System.out.println("Escolha um quarto: (1-50)");
             quarto_escolhido = in.nextInt();
             in.nextLine();
             for (Cliente cliente : clientes) {
 
                 if (cliente.getQuarto() == quarto_escolhido) {
-                    num = 2;
+                    num = 1; // NÃO ESQUECE DE TROCAR O NUM
                     break;
                 }
 
@@ -35,6 +36,72 @@ public class Reserva {
         }
 
         cl.setQuarto(quarto_escolhido);
+        data(in, cl, clientes);
+
+    }
+
+    public void data(Scanner in, Cliente cl, List<Cliente> clientes) {
+
+        Calendar calendario1;
+        Calendar calendario2;
+        new GregorianCalendar();
+        Calendar data_atual = Calendar.getInstance();
+        String data, datas[];
+        int dia, mes, ano;
+        dia = 0;
+        mes = 0;
+        ano = 0;
+
+        while (true) {
+
+            System.out.println("Escolha a data de entrada(dd/mm/yyyy): ");
+            data = in.nextLine();
+            datas = data.split("/");
+            dia = Integer.parseInt(datas[0]);
+            mes = Integer.parseInt(datas[1]) - 1;
+            ano = Integer.parseInt(datas[2]);
+
+            calendario1 = new GregorianCalendar(ano, mes, dia, 12, 0, 0);
+            System.out.println(calendario1.getTime());
+
+            System.out.println("Escolha a data de saída(dd/mm/yyyy): ");
+            data = in.nextLine();
+            datas = data.split("/");
+            dia = Integer.parseInt(datas[0]);
+            mes = Integer.parseInt(datas[1]) - 1;
+            ano = Integer.parseInt(datas[2]);
+
+            calendario2 = new GregorianCalendar(ano, mes, dia, 12, 0, 0);
+            System.out.println(calendario2.getTime());
+
+            for (Cliente cliente : clientes) {
+
+                if (cliente.getEntrada().compareTo(Calendar.getInstance()) < 0) {
+                    continue;
+                }
+                if (calendario1.compareTo(cliente.getSaida()) > 0 && calendario2.compareTo(cliente.getEntrada()) < 0) {
+
+                } else {
+                    System.out.println(cliente.getEntrada().getTime() + " - " + cliente.getSaida().getTime());
+                    System.out.println("Quarto indisponível!");
+                    break;
+                }
+            }
+
+            if (calendario2.compareTo(calendario1) > 0) {
+                break;
+            }
+
+            if (calendario1.compareTo(Calendar.getInstance()) < 0) {
+                System.out.println("Data inválida!");
+                new Reserva().data(in, cl, clientes);
+            }
+
+            System.out.println("Datas inválidas!");
+        }
+
+        cl.setEntrada(calendario1);
+        cl.setSaida(calendario2);
 
     }
 
