b57a6ada55b71a91985a65aab36af4c56dcbf1d4
João Victor <jvaraujocosta13@gmail.com>
2021-10-31 16:59:07 -0300

    Refatorando app

--- Gestão Hotel/src/App.java
+++ Gestão Hotel/src/App.java
@@ -1,12 +1,5 @@
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.Scanner;
 
 import Model.Cliente;
 import View.LoginView;
@@ -16,50 +9,10 @@ public class App {
 
         List<Cliente> clientes = new ArrayList<>();
         Cliente cl = new Cliente("admin", "123", "admin", "123", "123", "123");
+        cl.setAdmin(true);
         clientes.add(cl);
 
         new LoginView(clientes).login();
 
-
-        // Scanner in = new Scanner(System.in);
-        // List<Cliente> clientes = new ArrayList<>(50);
-
-        // String resp;
-        // resp = null;
-
-        
-
-        // while (resp != "s") {
-
-        //     System.out.println("A - Cadastrar");
-        //     System.out.println("B - Login");
-        //     System.out.println("S - Sair");
-            
-        //     resp = in.nextLine().toLowerCase();
-
-        //     switch (resp) {
-
-        //         case "a":
-        //             new Cadastro().cadastrar(clientes, in);
-        //             break;
-                
-        //         case "b":
-        //             new Login().login(clientes, in);
-        //             break;
-
-        //         case "s":
-        //             resp = "s";
-        //             break;
-
-        //         default:
-        //             System.out.println("Comando inválido!");
-        //             break;
-
-        //     }
-
-        // }
-
-
-        // in.close();
     }
 }
